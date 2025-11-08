c7398c27664a32c7cb9b825d4234c931dac8c8ca
PaesaggioSonoro <francesco.zanusfortes@gmail.com>
2025-11-08 15:08:12 +0100

    feat: better graphics, resizing

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function GameLayout({children}: {children: React.ReactNode}) {
-  return <div className="flex h-screen w-screen flex-col items-center justify-center">{children}</div>;
+  return (
+    <div className="min-h-screen w-full bg-white px-4 py-10 text-zinc-900 sm:px-8 lg:px-12">
+      <main className="mx-auto flex w-fit min-w-5xl flex-col gap-6 transition-all duration-300">{children}</main>
+    </div>
+  );
 }
 
 export default GameLayout;
